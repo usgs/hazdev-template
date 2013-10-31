47dbfcaec02a4e74f2fe354b0a54b0f5843f8a31
Jeremy Fee <jmfee@usgs.gov>
2013-10-31 12:45:04 -0600

    Removed default template css file from layout (now in _config.inc.php)

--- src/lib/layout.inc.php
+++ src/lib/layout.inc.php
@@ -4,7 +4,6 @@
 	<title><?php echo strip_tags($TITLE); ?></title>
 	<meta charset="utf-8"/>
 	<meta name="viewport" content="width=device-width, initial-scale=1"/>
-	<link rel="stylesheet" href="/template/css/index.css"/>
 	<?php
 
 		if ($HEAD) {
