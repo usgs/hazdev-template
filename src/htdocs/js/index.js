721173dbbed8a3fd0ebdc1b8330ba462656f75d7
Jeremy Fee <jmfee@usgs.gov>
2013-12-10 17:30:10 -0700

    Implemented and integrated js SocialLinks

--- src/htdocs/js/index.js
+++ src/htdocs/js/index.js
@@ -1,8 +1,23 @@
 
-
-require(['./OffCanvas'], function (OffCanvas) {
+require([
+	'./OffCanvas',
+	'./SocialLinks'
+], function (
+	OffCanvas,
+	SocialLinks
+) {
 	'use strict';
 
-	// enable offcanvas navigation
+	// make the site footer offcanvas
+	document.querySelector('.site-footer').classList.add('offcanvas');
 	new OffCanvas();
+
+	// create social links element
+	var socialEl = document.createElement('nav');
+	socialEl.id = 'page-social';
+	socialEl.className = 'page-social';
+	socialEl.setAttribute('aria-label', 'Share this page');
+	document.querySelector('.page-footer').appendChild(socialEl);
+	new SocialLinks({el: socialEl});
+
 });
