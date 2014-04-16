9f9ca4913e0f4da7473ce13c4cd415be0d187d6c
ehunter-usgs <ehunter@usgs.gov>
2014-04-16 12:08:05 -0600

    Add responsive tables styles to the template. Do not add them by default.

--- example/index.php
+++ example/index.php
@@ -63,3 +63,67 @@ quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
 cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
 proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+
+
+<table class="responsive test-table">
+	<thead>
+		<tr>
+			<th>First Header</th>
+			<th>Second Header</th>
+			<th>Third Header</th>
+			<th>Fourth Header</th>
+			<th>Fifth Header</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+		<tr>
+			<td>This</td>
+			<td>Is</td>
+			<td>A</td>
+			<td>Responsive</td>
+			<td>Table</td>
+		</tr>
+	</tbody>
+</table>
