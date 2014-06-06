36183a3ae7c7970874d70abf32add95691c0a752
Eddie McWhirter <emcwhirter@usgs.gov>
2014-06-06 10:33:28 -0600

    Add examples to jumplist and references from Lisa's code

--- example/references.php
+++ example/references.php
@@ -1,11 +1,35 @@
 <?php
 
 if (!isset($TEMPLATE)) {
-	$TITLE = 'References demo';
+	$TITLE = 'References';
 	$NAVIGATION = true;
-	$HEAD = '
-	<link rel="stylesheet" href="/css/references.css"/>
-	';
+	$HEAD = '<link rel="stylesheet" href="/css/references.css"/>';
 
-	include 'template.inc.php';
+	include('template.inc.php');
 }
+?>
+
+<p><a href="https://github.com/usgs/earthquake-website/blob/master/src/htdocs/css/references.scss">References SCSS</a></p>
+
+<ul class='referencelist'>
+	<li>Reference 1 - showing that the format is in bibliography style where the
+	second line is indented. Wald et al., Some really long paper title about a
+	very obscure topic in seismology, some scientific journal, series number,
+	date.</li>
+	<li>Reference 2 - showing that the format is in bibliography style where the
+	second line is indented. Wald et al., Some really long paper title about a
+	very obscure topic in seismology, some scientific journal, series number,
+	date.</li>
+	<li>Reference 3 - showing that the format is in bibliography style where the
+	second line is indented. Wald et al., Some really long paper title about a
+	very obscure topic in seismology, some scientific journal, series number,
+	date.</li>
+	<li>Reference 4 - showing that the format is in bibliography style where the
+	second line is indented. Wald et al., Some really long paper title about a
+	very obscure topic in seismology, some scientific journal, series number,
+	date.</li>
+	<li>Reference 5 - showing that the format is in bibliography style where the
+	second line is indented. Wald et al., Some really long paper title about a
+	very obscure topic in seismology, some scientific journal, series number,
+	date.</li>
+</ul>
