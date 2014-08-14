bc75f3a5a059c44f32da54d97fc5b7a0ec111068
ehunter-usgs <ehunter@usgs.gov>
2014-08-14 11:22:54 -0600

    Reduce header font-sizes. Use base font-size 10px and reimplement (r)ems.

--- example/typography.php
+++ example/typography.php
@@ -22,6 +22,13 @@ if (!isset($TEMPLATE)) {
 				border: 1px solid #ededed;
 			}
 
+			hr {
+				padding:0;
+				margin:0;
+				border:none;
+				border-top:1px solid #ccc;
+			}
+
 		</style>
 	';
 	$FOOT = '<script src="js/index.js"></script>';
@@ -97,40 +104,27 @@ Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
 deserunt mollit anim id est laborum.
 </p>
 
-
-<h2 class="underline">Nested Headers</h2>
-
+<h6>h6 Header</h6>
 <p>
-	This is an example of what happens when you nest headers inside
-	&lt;section&gt; tags.
+Lorem ipsum dolor sit amet, consectetur adipisici<strong>*</strong> g elit,
+sed do eius<strong>*</strong> od tempor<strong>*</strong> incididunt ut labore
+et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation
+ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia
+deserunt mollit anim id est laborum.
 </p>
 
-<pre class="code">
-&lt;h1&gt;h1 Header&lt;/h1&gt;
-&lt;section&gt;
-	&lt;h1&gt;h1 Header&lt;/h1&gt;
-	&lt;section&gt;
-		&lt;h1&gt;h1 Header&lt;/h1&gt;
-		&lt;section&gt;
-			&lt;h1&gt;h1 Header&lt;/h1&gt;
-			&lt;section&gt;
-				&lt;h1&gt;h1 Header&lt;/h1&gt;
-			&lt;/section&gt;
-		&lt;/section&gt;
-	&lt;/section&gt;
-&lt;/section&gt;
-</pre>
-
+<hr>
 <h1>h1 Header</h1>
-<section>
-	<h1>h1 Header</h1>
-	<section>
-		<h1>h1 Header</h1>
-		<section>
-			<h1>h1 Header</h1>
-			<section>
-				<h1>h1 Header</h1>
-			</section>
-		</section>
-	</section>
-</section>
+<hr>
+<h2>h2 Header</h2>
+<hr>
+<h3>h3 Header</h3>
+<hr>
+<h4>h4 Header</h4>
+<hr>
+<h5>h5 Header</h5>
+<hr>
+<h6>h6 Header</h6>
+<hr>
