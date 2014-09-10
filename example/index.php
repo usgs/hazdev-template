ad62b86ed0d68f0f354964bf29e6d38555341c26
ehunter-usgs <ehunter@usgs.gov>
2014-09-10 14:59:04 -0600

    Modify buttons.scss to include additional colors.

--- example/index.php
+++ example/index.php
@@ -125,14 +125,40 @@ if (!isset($TEMPLATE)) {
 	of class modifiers you can use for a consistent look with common button usage.
 </p>
 <h3 class="subheading">Example 1</h3>
+<button type="submit">Enabled</button>
+<br>
 <button type="submit" disabled>Disabled</button>
-<button type="submit">Click Me!</button>
 
 <h3 class="subheading">Example 2</h3>
 <button class="confirm" type="submit">Yes</button>
 <button class="cancel" type="submit">No</button>
+<br>
+<button class="confirm" type="submit" disabled>Yes</button>
+<button class="cancel" type="submit" disabled>No</button>
 
 <h3 class="subheading">Example 3</h3>
 <button class="confirm icon" type="submit">Confirm</button>
 <button class="cancel icon" type="submit">Cancel</button>
+<br>
+<button class="confirm icon" type="submit" disabled>Confirm</button>
+<button class="cancel icon" type="submit" disabled>Cancel</button>
+
+<h3 class="subheading">Example 4</h3>
+<button class="red" type="submit">Red</button>
+<button class="orange" type="submit">Orange</button>
+<button class="yellow" type="submit">Yellow</button>
+<button class="green" type="submit">Green</button>
+<button class="blue" type="submit">Blue</button>
+<br>
+<button class="red tint" type="submit">Red</button>
+<button class="orange tint" type="submit">Orange</button>
+<button class="yellow tint" type="submit">Yellow</button>
+<button class="green tint" type="submit">Green</button>
+<button class="blue tint" type="submit">Blue</button>
+<br>
+<button class="red" type="submit" disabled>Red</button>
+<button class="orange" type="submit" disabled>Orange</button>
+<button class="yellow" type="submit" disabled>Yellow</button>
+<button class="green" type="submit" disabled>Green</button>
+<button class="blue" type="submit" disabled>Blue</button>
 </section>
