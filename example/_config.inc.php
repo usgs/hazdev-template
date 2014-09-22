e0b038f00baa6404195df770232603d0929a0900
Eric Martinez <emartinez@usgs.gov>
2014-09-22 09:15:46 -0600

    Made themes toggleable based on a theme query string parameter.

--- example/_config.inc.php
+++ example/_config.inc.php
@@ -1,15 +1,5 @@
 <?php
 
-$SITE_URL = 'earthquake.usgs.gov';
-
-$SITE_SITENAV =
-		navItem('#earthquakes', 'Earthquakes') .
-		navItem('#hazards', 'Hazards') .
-		navItem('#data', 'Data') .
-		navItem('#learn', 'Learn') .
-		navItem('/flexible-grid.php', 'Monitoring') .
-		navItem('#research', 'Research');
-
 $SITE_COMMONNAV =
 		navItem('#home', 'Home') .
 		navItem('#aboutus', 'About Us') .
@@ -17,10 +7,58 @@ $SITE_COMMONNAV =
 		navItem('#legal', 'Legal') .
 		navItem('#partners', 'Partners');
 
-$HEAD = 
-		// site theme
-		'<link rel="stylesheet" href="/css/index.css"/>' .
+$THEME = (isset($_GET['theme'])) ? $_GET['theme'] : 'default';
 
+if ($THEME === 'earthquake') {
+	$THEME_CSS =
+		'<link rel="stylesheet" href="/theme/site/earthquake/index.css"/>';
+
+	$SITE_URL = 'earthquake.usgs.gov';
+	$SITE_SITENAV =
+		navItem('#earthquakes', 'Earthquakes') .
+		navItem('#hazards', 'Hazards') .
+		navItem('#data', 'Data') .
+		navItem('#learn', 'Learn') .
+		navItem('/flexible-grid.php', 'Monitoring') .
+		navItem('#research', 'Research');
+} else if ($THEME === 'geomag') {
+	$THEME_CSS =
+		'<link rel="stylesheet" href="/theme/site/geomag/index.css"/>';
+
+	$SITE_URL = 'geomag.usgs.gov';
+	$SITE_SITENAV =
+		navItem('#monitoring', 'Monitoring') .
+		navItem('#data', 'Data &amp; Products') .
+		navItem('#research', 'Research') .
+		navItem('#learn', 'Learn') .
+		navItem('#services', 'Services') .
+		navItem('#partners', 'Partners');
+} else if ($THEME === 'landslides') {
+	$THEME_CSS =
+		'<link rel="stylesheet" href="/theme/site/landslides/index.css"/>';
+
+	$SITE_URL = 'landslides.usgs.gov';
+	$SITE_SITENAV =
+		navItem('#hazards', 'Hazards') .
+		navItem('#monitoring', 'Monitoring') .
+		navItem('#state_local', 'State &amp; Local') .
+		navItem('#learn', 'Learn') .
+		navItem('#research', 'Research');
+} else {
+	$THEME_CSS =
+		'<link rel="stylesheet" href="/theme/site/default/index.css"/>';
+
+	$SITE_URL = 'www.usgs.gov';
+	$SITE_SITENAV =
+		navItem('#navitem1', 'Nav Item 1') .
+		navItem('#navitem2', 'Nav Item 2') .
+		navItem('#navitem3', 'Nav Item 3') .
+		navItem('#navitem4', 'Nav Item 4') .
+		navItem('#navitem5', 'Nav Item 5') .
+		navItem('#navitem6', 'Nav Item 6');
+}
+
+$HEAD = $THEME_CSS .
 		// page head content
 		($HEAD ? $HEAD : '') .
 
