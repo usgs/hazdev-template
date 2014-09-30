0af323463f153751e7947d277480b8718b1a4840
ehunter-usgs <ehunter@usgs.gov>
2014-09-30 17:05:04 -0600

    Add html markup that can be styled as a demonstration.

--- example/text.php
+++ example/text.php
@@ -3,6 +3,70 @@
 		$TITLE = 'Text Level Semantics';
 		$NAVIGATION = true;
 
+		$HEAD = '
+			<link rel="stylesheet" href="/css/responsive-table.css"/>
+
+			<style>
+				pre > code {
+					padding: 10px;
+				}
+			</style>
+		';
+
 		include 'template.inc.php';
 	}
 ?>
+
+
+
+<h2>Text-level Semantics</h2>
+<p>
+	The following list gives you a short overview of the most commonly used text-level semantics and how to utilize them.
+</p>
+<h3 class="subheading">Example</h3>
+<table class="tabular">
+	<thead>
+		<tr>
+			<th>Element</th>
+			<th>Description</th>
+		</tr>
+	</thead>
+	<tbody>
+		<tr>
+			<td><code>&lt;a&gt;</code></td>
+			<td>Turn text into hypertext using the <a href="#">a element</a>.</td>
+		</tr>
+		<tr>
+			<td><code>&lt;em&gt;</code></td>
+			<td>Emphasize text using the <em>em element</em>.</td>
+		</tr>
+		<tr>
+			<td><code>&lt;strong&gt;</code></td>
+			<td>Imply any extra importance using the <strong>strong element</strong>.</td>
+		</tr>
+		<tr>
+			<td><code>&lt;code&gt;</code></td>
+			<td>
+				Define inline code snippets using the <code>code element</code>.
+			</td>
+		</tr>
+		<tr>
+			<td><code>&lt;mark&gt;</code></td>
+			<td>Highlight text with no semantic meaning using the <mark>mark element</mark>.</td>
+		</tr>
+		<tr>
+			<td><code>&lt;abbr&gt;</code></td>
+			<td>Define an abbreviation using the <abbr title="Abbreviation Element">abbr element</abbr> with a title.</td>
+		</tr>
+		<tr>
+			<td><code>&lt;small&gt;</code></td>
+			<td>De-emphasize text for small print using the <small>small element</small>.</td>
+		</tr>
+	</tbody>
+</table>
+<h3 class="subheading">Usage</h3>
+<pre>
+<code>
+	&lt; TODO:: add code sample.&gt;
+</code>
+</pre>
