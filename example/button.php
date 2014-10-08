659cb8998f817e911707d911265e56d0a552b923
ehunter-usgs <ehunter@usgs.gov>
2014-10-08 16:22:23 -0600

    Add some documentation to buttons.

--- example/button.php
+++ example/button.php
@@ -20,25 +20,30 @@ if (!isset($TEMPLATE)) {
 	Here are some examples of default styles for buttons, and also some examples
 	of class modifiers you can use for a consistent look with common button usage.
 </p>
-<h3 class="subheading">Example 1</h3>
+<h2 class="subheading">Example 1</h2>
 <p>
 	This is the default style applied to all buttons, it also displays the disabled state. The disabled state can be set using the disabled attribute or by applying the class <code>.disabled</code>.
 </p>
 <button>Enabled</button>
 <br>
 <button disabled="true">Disabled</button>
-<h3 class="subheading">Usage</h3>
+<h2 class="subheading">Usage</h2>
 <pre><code>&lt;button&gt;Enabled&lt;button&gt;
 &lt;button disabled="true"&gt;Disabled&lt;button&gt;
 </code></pre>
 
-<h3 class="subheading">Example 2</h3>
+
+<h2 class="subheading">Example 2</h2>
+<p>
+	When color styles are applied to buttons the disabled state does not
+	inherit the background color.
+</p>
 <button class="green">Yes</button>
 <button>No</button>
 <br>
 <button class="green" disabled="true">Yes</button>
 <button disabled="true">No</button>
-<h3 class="subheading">Usage</h3>
+<h2 class="subheading">Usage</h2>
 <pre><code>&lt;button class="green"&gt;Yes&lt;button&gt;
 &lt;button&gt;No&lt;button&gt;
 &lt;br&gt;
@@ -46,13 +51,18 @@ if (!isset($TEMPLATE)) {
 &lt;button disabled="true"&gt;No&lt;button&gt;
 </code></pre>
 
-<h3 class="subheading">Example 3</h3>
+<h2 class="subheading">Example 3</h2>
+<p>
+	There are two classes that can be applied to buttons for use with a yes/no
+	type of response. These classes are <code>.confirm</code> and
+	<code>.cancel</code> and they add an icon to the button.
+</p>
 <button class="green confirm">Yes</button>
 <button class="cancel">No</button>
 <br>
 <button class="green confirm" disabled="true">Yes</button>
 <button class="cancel" disabled="true">No</button>
-<h3 class="subheading">Usage</h3>
+<h2 class="subheading">Usage</h2>
 <pre><code>&lt;button class="confirm green"&gt;Yes&lt;button&gt;
 &lt;button class="cancel"&gt;No&lt;button&gt;
 &lt;br&gt;
@@ -60,7 +70,13 @@ if (!isset($TEMPLATE)) {
 &lt;button class="cancel" disabled="true"&gt;No&lt;button&gt;
 </code></pre>
 
-<h3 class="subheading">Example 4</h3>
+<h2 class="subheading">Example 4</h2>
+<p>
+	There are five predefined colors that can be applied to buttons. If you add
+	the class <code>.red</code>, <code>.orange</code>, <code>.yellow</code>,
+	<code>.green</code>, or <code>.blue</code>, then the button will be styled
+	with that color scheme. 
+</p>
 <button class="red">Red</button>
 <button class="orange">Orange</button>
 <button class="yellow">Yellow</button>
@@ -73,7 +89,7 @@ if (!isset($TEMPLATE)) {
 <button class="green" disabled="true">Green</button>
 <button class="blue" disabled="true">Blue</button>
 </section>
-<h3 class="subheading">Usage</h3>
+<h2 class="subheading">Usage</h2>
 <pre><code>&lt;button class="red"&gt;Red&lt;button&gt;
 &lt;button class="orange"&gt;Orange&lt;button&gt;
 &lt;button class="yellow"&gt;Yellow&lt;button&gt;
