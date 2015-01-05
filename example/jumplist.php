5b21d239fccf4a8370689c1f11d5dcbc0a0d765c
ehunter-usgs <ehunter@usgs.gov>
2015-01-05 10:57:51 -0700

    Fix wrapping issue on jump links. Use border instead of pipe to separate links.

--- example/jumplist.php
+++ example/jumplist.php
@@ -12,11 +12,15 @@ if (!isset($TEMPLATE)) {
 <p><a href="https://github.com/usgs/hazdev-template/tree/master/src/htdocs/css/jumplist.scss">JumpList SCSS</a></p>
 
 <nav class="jumplist">
-	<a href="#aheader">Jump to Here</a>
-	<a href="#another">Jump to There</a>
+	<a href="#one">Jump to One</a>
+	<a href="#two">Jump to Two</a>
+	<a href="#three">Jump to Three</a>
+	<a href="#four">Jump to Four</a>
+	<a href="#five">Jump to Five</a>
+	<a href="#six">Jump to Six</a>
 </nav>
 
-<h2 id="aheader">Jump to Here</h2>
+<h2 id="one">Jump to One</h2>
 
 <p>
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
@@ -43,7 +47,7 @@ if (!isset($TEMPLATE)) {
 	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 </p>
 
-<h2 id="another">Jump to There</h2>
+<h2 id="two">Jump to Two</h2>
 
 <p>Lorem ipsum dolor sit amet, consectetur
 	adipisicing elit, sed do eiusmod
@@ -87,6 +91,54 @@ if (!isset($TEMPLATE)) {
 </p>
 <p>
 	Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+
+<h2 id="three">Jump to Three</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur
+	adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+
+<h2 id="four">Jump to Four</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur
+	adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+
+<h2 id="five">Jump to Five</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur
+	adipisicing elit, sed do eiusmod
+	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+	cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+	proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+</p>
+
+
+<h2 id="six">Jump to Six</h2>
+
+<p>Lorem ipsum dolor sit amet, consectetur
+	adipisicing elit, sed do eiusmod
 	tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 	quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
 	consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
