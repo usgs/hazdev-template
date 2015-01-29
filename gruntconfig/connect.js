ef665e533b91502feb6cf0000593a1ea681f104f
Jeremy Fee <jmfee@usgs.gov>
2015-01-29 15:40:24 -0700

    Update connect configuration

--- gruntconfig/connect.js
+++ gruntconfig/connect.js
@@ -1,32 +1,6 @@
 'use strict';
 
-var config = require('./config'),
-    path = require('path');
-
-
-var getProxy = function () {
-  var proxy = require('grunt-connect-proxy/lib/utils');
-  return proxy.proxyRequest;
-};
-
-var mountFolder = function (connect, dir) {
-  return connect.static(path.resolve(dir));
-};
-
-var mountPHP = function (dir, options) {
-  options = options || {
-    'phpini': config.src + '/conf/php.ini'
-  };
-  var gateway = require('gateway');
-  var gatewayOptions = {
-    '.php': 'php-cgi',
-    'env': {
-      'PHPRC': process.cwd() + '/' + options.phpini
-    }
-  };
-
-  return gateway(path.resolve(dir), gatewayOptions);
-};
+var config = require('./config');
 
 
 var connect = {
@@ -37,40 +11,43 @@ var connect = {
     proxies: [{
       context: '/theme',
       host: 'localhost',
-      https: false,
       port: '<%= connect.devTemplate.options.port %>',
-      changeOrigin: false,
-      xforward: false,
       rewrite: {'/theme': ''}
     }],
     options: {
-      base: config.example,
+      base: [config.example],
       port: 8000,
       open: 'http://localhost:8000/',
       livereload: true,
-      middleware: function (connect, options) {
-        return [
-          getProxy(),
-          mountPHP(options.base[0], {
-            phpini: config.build + '/' + config.src + '/conf/php.ini'
-          }),
-          mountFolder(connect, options.base[0])
-        ];
+      middleware: function (connect, options, middlewares) {
+        middlewares.unshift(
+          require('grunt-connect-proxy/lib/utils').proxyRequest,
+          require('gateway')(options.base[0], {
+            '.php': 'php-cgi',
+            'env': {
+              'PHPRC': config.build + '/' + config.src + '/conf/php.ini'
+            }
+          })
+        );
+        return middlewares;
       }
     }
   },
 
   devTemplate: {
     options: {
-      base: config.build + '/' + config.src + '/htdocs',
+      base: [config.build + '/' + config.src + '/htdocs'],
       port: 8003,
-      middleware: function (connect, options) {
-        return [
-          mountPHP(options.base[0], {
-            phpini: config.build + '/' + config.src + '/conf/php.ini'
-          }),
-          mountFolder(connect, options.base[0])
-        ];
+      middleware: function (connect, options, middlewares) {
+        middlewares.unshift(
+          require('gateway')(options.base[0], {
+            '.php': 'php-cgi',
+            'env': {
+              'PHPRC': config.build + '/' + config.src + '/conf/php.ini'
+            }
+          })
+        );
+        return middlewares;
       }
     }
   },
@@ -79,36 +56,43 @@ var connect = {
     proxies: [{
       context: '/theme',
       host: 'localhost',
-      https: false,
       port: '<%= connect.distTemplate.options.port %>',
-      changeOrigin: false,
-      xforward: false,
       rewrite: {'/theme': ''}
     }],
     options: {
-      base: config.example,
+      base: [config.example],
       port: 8002,
       open: 'http://localhost:8002/',
       keepalive: true,
-      middleware: function (connect, options) {
-        return [
-          getProxy(),
-          mountPHP(options.base[0], {phpini: config.dist + '/conf/php.ini'}),
-          mountFolder(connect, options.base[0])
-        ];
+      middleware: function (connect, options, middlewares) {
+        middlewares.unshift(
+          require('grunt-connect-proxy/lib/utils').proxyRequest,
+          require('gateway')(options.base[0], {
+            '.php': 'php-cgi',
+            'env': {
+              'PHPRC': config.dist + '/conf/php.ini'
+            }
+          })
+        );
+        return middlewares;
       }
     }
   },
 
   distTemplate: {
     options: {
-      base: config.dist + '/htdocs',
+      base: [config.dist + '/htdocs'],
       port: 8003,
-      middleware: function (connect, options) {
-        return [
-          mountPHP(options.base[0], {phpini: config.dist + '/conf/php.ini'}),
-          mountFolder(connect, options.base[0])
-        ];
+      middleware: function (connect, options, middlewares) {
+        middlewares.unshift(
+          require('gateway')(options.base[0], {
+            '.php': 'php-cgi',
+            'env': {
+              'PHPRC': config.dist + '/conf/php.ini'
+            }
+          })
+        );
+        return middlewares;
       }
     }
   }
