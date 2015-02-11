c9bc1c8f7947398990eb035746a9d5259d34201e
lisa-usgs <lisa@usgs.gov>
2015-02-11 15:45:03 -0700

    fixed additional alignment and tag issues
    fixed alignment of all list tags, closed all list tags, changed H2 to
    h2, made sure nothing wraps past column 80!

--- example/content.php
+++ example/content.php
@@ -17,9 +17,14 @@ if (!isset($TEMPLATE)) {
 <h2>Links and Labels</h2>
 <ul>
   <li>Never use &ldquo;click here&rdquo; for a link.</li>
-  <li>The link text should match the title of the webpage you're linking to.</li>
+  <li>
+    The link text should match the title of the webpage you're linking to.
+  </li>
   <li>Use title case for navigation links.</li>
-  <li>off-site links - will show symbol, do not make open in new window with <code>target=&ldquo;_blank&rdquo;</code>.</li>
+  <li>
+    off-site links - will show symbol, do not make open in new window with
+    <code>target=&ldquo;_blank&rdquo;</code>.
+  </li>
 </ul>
 
 <h2>Maps</h2>
@@ -40,14 +45,22 @@ if (!isset($TEMPLATE)) {
 
 <h2>Navigation</h2>
 <ul>
-  <li>Site navigation will always appear off-canvas on small-format and at the
-  bottom-left on large-format.</li>
-  <li>If local navigation is needed, it will appear above the site navigation.</li>
-  <li>If the webpage is more than one level below the site section, a navigation
-  link to the level one above the current one will appear above the site
-  navigation  (and below the local navigation if it exists).
-  <li>Navigation text should match the title of the webpage you&rsquo;re linking to.
-  If it is too long, you can shorten it but don't change any of the words.</li>
+  <li>
+    Site navigation will always appear off-canvas on small-format and at the
+    bottom-left on large-format.
+  </li>
+  <li>
+    If local navigation is needed, it will appear above the site navigation.
+  </li>
+  <li>
+    If the webpage is more than one level below the site section, a navigation
+    link to the level one above the current one will appear above the site
+    navigation  (and below the local navigation if it exists).
+  </li>
+  <li>
+    Navigation text should match the title of the webpage you&rsquo;re linking
+    to. If it is too long, you can shorten it but don't change any of the words.
+  </li>
 </ul>
 
 <h2>Titles and Headers</h2>
@@ -59,34 +72,45 @@ if (!isset($TEMPLATE)) {
 
 <h2>Writing Style</h2>
 <ul>
-  <li>Include at top of all webpages (example):
-  <pre>
-    <code>
-    // Webpage contact - Lisa Wald
-    // Content contact/SME - Art Frankel
-    // Created Jan 2015
-    // Last updated 01/30/2015
-    </code>
-  </pre>
+  <li>
+    Include at top of all webpages (example):
+    <pre>
+      <code>
+        // Webpage contact - Lisa Wald
+        // Content contact/SME - Art Frankel
+        // Created Jan 2015
+        // Last updated 01/30/2015
+      </code>
+    </pre>
   </li>
-  <li>Use the <a href="http://www.plainlanguage.gov/">Plain Language</a>
-      guidelines.</li>
-  <li>&ldquo;and&rdquo; versus &ldquo;&amp;&rdquo; - always use &ldquo;and&rdquo;
-      in copy, and in headers and
-      navigation links as much as possible, instead of &ldquo;&amp;&rdquo;.
+  <li>
+    Use the <a href="http://www.plainlanguage.gov/">Plain Language</a>
+    guidelines.
+  </li>
+  <li>
+    &ldquo;and&rdquo; versus &ldquo;&amp;&rdquo; - always use &ldquo;and&rdquo;
+      in copy, and in headers and navigation links as much as possible, instead
+      of &ldquo;&amp;&rdquo;.
+  </li>
   <li>U.S. not US.</li>
-  <li>USGS not U.S.G.S., U.S. &ldquo;Geological Survey&rdquo;, &ldquo;US
-      Geological Survey&rdquo;.</li>
-  <li>&ldquo;University of Washington&rdquo; is preferred to &ldquo;Univ. of
-      Washington&rdquo; (include
-      &ldquo;.&rdquo; here).</li>
+  <li>
+    USGS not U.S.G.S., &ldquo;U.S. Geological Survey&rdquo;, &ldquo;US
+    Geological Survey&rdquo;.
+  </li>
+  <li>
+    &ldquo;University of Washington&rdquo; is preferred to &ldquo;Univ. of
+      Washington&rdquo; (include &ldquo;.&rdquo; here).
+  </li>
   <li>&ldquo;real-time&rdquo; not &ldquo;realtime&rdquo;.</li>
-  <li>phone numbers - xxx-xxx-xxxx
-  <li>5000 not 5,000, but 10,000+ gets the comma.
-  <li>voice and purpose:
+  <li>phone numbers - xxx-xxx-xxxx</li>
+  <li>5000 not 5,000, but 10,000+ gets the comma.</li>
+  <li>
+    voice and purpose:
     <ul>
-      <li>make science about people too, not just data and research, include
-          photos of people.</li>
+      <li>
+        make science about people too, not just data and research, include
+        photos of people.
+      </li>
       <li>explain scientific method by example.</li>
       <li>teach something new about the science, not just the results.</li>
       <li>think &ldquo;fact sheet&rdquo; for level.</li>
@@ -98,8 +122,10 @@ if (!isset($TEMPLATE)) {
   <li>width of text on page will be limited to ??? using the class ?</li>
 </ul>
 
-<H2>See also:</h2>
+<h2>See also:</h2>
 <ul>
-  <li><a  href="https://github.com/usgs/hazdev-template/">Website Overview,
-      Config, Variables, Functions, and SCSS Patterns</a>
+  <li>
+    <a href="https://github.com/usgs/hazdev-template/">Website Overview,
+        Config, Variables, Functions, and SCSS Patterns</a>
+  </li>
 </ul>
