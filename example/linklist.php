595766af1811514869b9960b0ba226a17bcb970e
Scott Haefner <shaefner@evpn-standard-2-255.cr.usgs.gov>
2015-03-20 11:50:44 -0700

    change divider color; add link list

--- example/linklist.php
+++ example/linklist.php
@@ -10,6 +10,8 @@ if (!isset($TEMPLATE)) {
 
 ?>
 
+<h2>Image List</h2>
+
 <ul class="linklist">
   <li>
     <a href="#1">
@@ -55,3 +57,32 @@ if (!isset($TEMPLATE)) {
     </a>
   </li>
 </ul>
+
+<h2>Link List</h2>
+
+<ul class="linklist">
+  <li>
+    <a href="#1">
+      <h4>Featured Research</h4>
+      <p>On August 23, 2011 tens of millions of people along the East Coast of
+        the U.S from Maine to Georgia, west to Cincinnati, Cleveland, and
+        Chicago, and southeastern Canada suddenly felt the earth shaking from
+        the largest earthquake in the eastern U.S. since the M5.8 earthquake in
+        1944 near Cornwall and Massena, New York. What have scientists learned
+        from this rare earthquake?
+      </p>
+    </a>
+  </li>
+  <li>
+    <a href="#2">
+      <h4>Was the M5.3 Trinidad, CO Earthquake Natural or Induced?</h4>
+      <p>A magnitude 5.3 earthquake occurred near Trinidad, Colorado in the
+        Raton Basin on August of 2011. A previous study by USGS scientists of
+        smaller earthquakes in the area had concluded that the Raton Basin was
+        experiencing a significant increase in the number of earthquakes
+        starting in 2001 after an increase in hydrocarbon production activity
+        starting in 1999. The M5.3 Trinidad earthquake provided another
+        important piece to the puzzle.
+      </p>
+    </a>
+  </li>
