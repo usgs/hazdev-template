25feca93cb60070fe0751d11f0e35ba85e1ea9c6
ehunter-usgs <ehunter@usgs.gov>
2015-09-10 14:57:42 -0600

    Remove comments, cannot use variables in file names

--- gruntconfig/postcss.js
+++ gruntconfig/postcss.js
@@ -38,26 +38,6 @@ var postcss = {
           'src/htdocs/site/geomag/index.scss',
       '.build/src/htdocs/site/landslides/index.css':
           'src/htdocs/site/landslides/index.scss'
-
-      // config.build + '/' + config.src + '/htdocs/css/alert.css':
-      //     config.src + '/htdocs/css/alert.css',
-      // config.build + '/' + config.src + '/htdocs/css/flexible-grid.css':
-      //     config.src + '/htdocs/css/flexible-grid.css',
-      // config.build + '/' + config.src + '/htdocs/css/jumplist.css':
-      //     config.src + '/htdocs/css/jumplist.css',
-      // config.build + '/' + config.src + '/htdocs/css/references.css':
-      //     config.src + '/htdocs/css/references.css',
-      // config.build + '/' + config.src + '/htdocs/css/responsive-table.css':
-      //     config.src + '/htdocs/css/responsive-table.css',
-
-      // config.build + '/' + config.src + '/htdocs/site/default/index.css':
-      //     config.src + '/htdocs/site/default/index.css',
-      // config.build + '/' + config.src + '/htdocs/site/earthquake/index.css':
-      //     config.src + '/htdocs/site/earthquake/index.css',
-      // config.build + '/' + config.src + '/htdocs/site/geomag/index.css':
-      //     config.src + '/htdocs/site/geomag/index.css',
-      // config.build + '/' + config.src + '/htdocs/site/landslides/index.css':
-      //     config.src + '/htdocs/site/landslides/index.css'
     }
   },
 
@@ -87,26 +67,6 @@ var postcss = {
           '.build/src/htdocs/site/geomag/index.css',
       'dist/htdocs/site/landslides/index.css':
           '.build/src/htdocs/site/landslides/index.css'
-
-      // config.dist + '/htdocs/css/alert.css':
-      //     config.build + '/' + config.src + '/htdocs/css/alert.css',
-      // config.dist + '/htdocs/css/flexible-grid.css':
-      //     config.build + '/' + config.src + '/htdocs/css/flexible-grid.css',
-      // config.dist + '/htdocs/css/jumplist.css':
-      //     config.build + '/' + config.src + '/htdocs/css/jumplist.css',
-      // config.dist + '/htdocs/css/references.css':
-      //     config.build + '/' + config.src + '/htdocs/css/references.css',
-      // config.dist + '/htdocs/css/responsive-table.css':
-      //     config.build + '/' + config.src + '/htdocs/css/responsive-table.css',
-
-      // config.dist + '/htdocs/site/default/index.css':
-      //     config.build + '/' + config.src + '/htdocs/site/default/index.css',
-      // config.dist + '/htdocs/site/earthquake/index.css':
-      //     config.build + '/' + config.src + '/htdocs/site/earthquake/index.css',
-      // config.dist + '/htdocs/site/geomag/index.css':
-      //     config.build + '/' + config.src + '/htdocs/site/geomag/index.css',
-      // config.dist + '/htdocs/site/landslides/index.css':
-      //     config.build + '/' + config.src + '/htdocs/site/landslides/index.css'
     }
   }
 };
