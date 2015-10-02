e206a285f0ca0c67c5a3fd1beffb9b7c330cad36
ehunter-usgs <ehunter@usgs.gov>
2015-10-02 10:20:55 -0600

    Update tables, add borderless table, remove styles for responsive tables. Add scrolling classes to make lists and tables more responsive

--- example/responsive-table.php
+++ example/responsive-table.php
@@ -1,7 +1,7 @@
 <?php
 
 if (!isset($TEMPLATE)) {
-	$TITLE = 'Responsive Table Demo';
+	$TITLE = 'Tables';
 	$NAVIGATION = true;
 
 	$HEAD = '
@@ -26,19 +26,17 @@ if (!isset($TEMPLATE)) {
 
 ?>
 
-
-<h2>Table (.tabular)</h2>
 <p>
-	This is a non-responsive table styled with the <code>.tabular</code> class.
+	Tables display tabular data in columns and rows.
 </p>
-<table class="tabular">
+
+<table>
 	<thead>
 		<tr>
 			<th>First Header</th>
 			<th>Second Header</th>
 			<th>Third Header</th>
 			<th>Fourth Header</th>
-			<th>Fifth Header</th>
 		</tr>
 	</thead>
 	<tbody>
@@ -46,49 +44,42 @@ if (!isset($TEMPLATE)) {
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 		<tr>
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 		<tr>
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 		<tr>
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 		<tr>
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 		<tr>
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 		<tr>
 			<td>This</td>
 			<td>Is</td>
 			<td>A</td>
-			<td>Responsive</td>
 			<td>Table</td>
 		</tr>
 	</tbody>
@@ -101,7 +92,6 @@ if (!isset($TEMPLATE)) {
 			&lt;th&gt;Second Header&lt;/th&gt;
 			&lt;th&gt;Third Header&lt;/th&gt;
 			&lt;th&gt;Fourth Header&lt;/th&gt;
-			&lt;th&gt;Fifth Header&lt;/th&gt;
 		&lt;/tr&gt;
 	&lt;/thead&gt;
 	&lt;tbody&gt;
@@ -109,7 +99,6 @@ if (!isset($TEMPLATE)) {
 			&lt;td&gt;This&lt;/td&gt;
 			&lt;td&gt;Is&lt;/td&gt;
 			&lt;td&gt;A&lt;/td&gt;
-			&lt;td&gt;Responsive&lt;/td&gt;
 			&lt;td&gt;Table&lt;/td&gt;
 		&lt;/tr&gt;
 	&hellip;
@@ -118,57 +107,12 @@ if (!isset($TEMPLATE)) {
 </code></pre>
 
 
-<h2>Vertical Table (.tabular)</h2>
-<p>
-	This is a non-responsive table styled with the <code>.tabular</code> class.
-</p>
-<table class="tabular">
-	<tbody>
-		<tr>
-			<th>First Header</th>
-			<td>This</td>
-		</tr>
-		<tr>
-			<th>Second Header</th>
-			<td>Is</td>
-		</tr>
-		<tr>
-			<th>Third Header</th>
-			<td>A</td>
-		</tr>
-		<tr>
-			<th>Fourth Header</th>
-			<td>Vertical</td>
-		</tr>
-		<tr>
-			<th>Fifth Header</th>
-			<td>Table</td>
-		</tr>
-	</tbody>
-</table>
-<h3>Usage</h3>
-<pre><code>&lt;table class="<strong>tabular</strong>"&gt;
-	&lt;tbody&gt;
-		&lt;tr&gt;
-			&lt;th&gt;First Header&lt;/th&gt;
-			&lt;td&gt;This&lt;/td&gt;
-		&lt;/tr&gt;
-		&lt;tr&gt;
-			&lt;th&gt;Second Header&lt;/th&gt;
-			&lt;td&gt;Is&lt;/td&gt;
-		&lt;/tr&gt;
-		&hellip;
-	&lt;/tbody&gt;
-&lt;/table&gt;
-</code></pre>
 
-
-<h2>Responsive Table (.tabular .responsive)</h2>
+<h2>Borderless Table (.borderless)</h2>
 <p>
-	Responsive table using the <code>.tabular .responsive</code> classes.
-	This table has no th elements in the body.
+	This table is styled using the .borderless class. 
 </p>
-<table class="tabular responsive test-horizontal">
+<table class="borderless">
 	<thead>
 		<tr>
 			<th>First Header</th>
@@ -255,83 +199,87 @@ if (!isset($TEMPLATE)) {
 </code></pre>
 
 
-<h2>Responsive Table (.tabular .responsive)</h2>
+<h2>Responsive Table (.horizontal-scrolling)</h2>
 <p>
-	Responsive table using the <code>.tabular .responsive</code> classes.
-	This table has a leading <code>th</code> element at the first position
-	of each row in the body.
+	This table has been made responsive by adding the .horizontal-scrolling
+	class to a div wrapper element.
 </p>
-<table class="tabular responsive test-horizontal">
-	<thead>
-		<tr>
-			<th>First Header</th>
-			<th>Second Header</th>
-			<th>Third Header</th>
-			<th>Fourth Header</th>
-			<th>Fifth Header</th>
-		</tr>
-	</thead>
-	<tbody>
-		<tr>
-			<th>1st Value</th>
-			<td>In</td>
-			<td>A</td>
-			<td>Responsive</td>
-			<td>Table</td>
-		</tr>
-		<tr>
-			<th>2nd Value</th>
-			<td>In</td>
-			<td>A</td>
-			<td>Responsive</td>
-			<td>Table</td>
-		</tr>
-		<tr>
-			<th>3rd Value</th>
-			<td>In</td>
-			<td>A</td>
-			<td>Responsive</td>
-			<td>Table</td>
-		</tr>
-		<tr>
-			<th>4th Value</th>
-			<td>In</td>
-			<td>A</td>
-			<td>Responsive</td>
-			<td>Table</td>
-		</tr>
-		<tr>
-			<th>5th Value</th>
-			<td>In</td>
-			<td>A</td>
-			<td>Responsive</td>
-			<td>Table</td>
-		</tr>
-	</tbody>
-</table>
-<h3>Usage</h3>
-<pre><code>&lt;table class="<strong>tabular responsive</strong>"&gt;
-	&lt;thead&gt;
-		&lt;tr&gt;
-			&lt;th&gt;First Header&lt;/th&gt;
-			&lt;th&gt;Second Header&lt;/th&gt;
-			&lt;th&gt;Third Header&lt;/th&gt;
-			&lt;th&gt;Fourth Header&lt;/th&gt;
-			&lt;th&gt;Fifth Header&lt;/th&gt;
-		&lt;/tr&gt;
-	&lt;/thead&gt;
-	&lt;tbody&gt;
-		&lt;tr&gt;
-			<strong>&lt;th&gt;</strong>This<strong>&lt;/th&gt;</strong>
-			&lt;td&gt;Is&lt;/td&gt;
-			&lt;td&gt;A&lt;/td&gt;
-			&lt;td&gt;Responsive&lt;/td&gt;
-			&lt;td&gt;Table&lt;/td&gt;
-		&lt;/tr&gt;
-	&hellip;
-	&lt;/tbody&gt;
-&lt;/table&gt;
-</code></pre>
+
+<div class="horizontal-scrolling">
+	<table>
+		<thead>
+			<tr>
+				<th>First Header</th>
+				<th>Second Header</th>
+				<th>Third Header</th>
+				<th>Fourth Header</th>
+				<th>Fifth Header</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<th>1st Value</th>
+				<td>In</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<th>2nd Value</th>
+				<td>In</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<th>3rd Value</th>
+				<td>In</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<th>4th Value</th>
+				<td>In</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<th>5th Value</th>
+				<td>In</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+		</tbody>
+	</table>
+	<h3>Usage</h3>
+	<pre><code>&lt;div class="horizontal-scrolling"&gt;
+		&lt;table class="<strong>tabular responsive</strong>"&gt;
+			&lt;thead&gt;
+				&lt;tr&gt;
+					&lt;th&gt;First Header&lt;/th&gt;
+					&lt;th&gt;Second Header&lt;/th&gt;
+					&lt;th&gt;Third Header&lt;/th&gt;
+					&lt;th&gt;Fourth Header&lt;/th&gt;
+					&lt;th&gt;Fifth Header&lt;/th&gt;
+				&lt;/tr&gt;
+			&lt;/thead&gt;
+			&lt;tbody&gt;
+				&lt;tr&gt;
+					<strong>&lt;th&gt;</strong>This<strong>&lt;/th&gt;</strong>
+					&lt;td&gt;Is&lt;/td&gt;
+					&lt;td&gt;A&lt;/td&gt;
+					&lt;td&gt;Responsive&lt;/td&gt;
+					&lt;td&gt;Table&lt;/td&gt;
+				&lt;/tr&gt;
+			&hellip;
+			&lt;/tbody&gt;
+		&lt;/table&gt;
+	&lt;/div&gt;
+	</code></pre>
+</div>
 
 <h2>Responsive Table (.tabular .responsive .center)</h2>
 <p>
---
+++ example/table.php
@@ -0,0 +1,294 @@
+<?php
+
+if (!isset($TEMPLATE)) {
+	$TITLE = 'Tables';
+	$NAVIGATION = true;
+
+	$HEAD = '
+	<link rel="stylesheet" href="/theme/css/responsive-table.css"/>
+	<style>
+		@media screen and (max-width: 559px) {
+			.test-horizontal td:nth-of-type(1):before { content: "First Header"; }
+			.test-horizontal td:nth-of-type(2):before { content: "Second Header"; }
+			.test-horizontal td:nth-of-type(3):before { content: "Third Header"; }
+			.test-horizontal td:nth-of-type(4):before { content: "Fourth Header"; }
+			.test-horizontal td:nth-of-type(5):before { content: "Fifth Header"; }
+		}
+
+		code > strong {
+			/* color: red */
+			color: #E46554;
+		}
+	</style>
+	';
+	include 'template.inc.php';
+}
+
+?>
+
+<p>
+	Tables display tabular data in columns and rows.
+</p>
+
+<div class="horizontal-scrolling">
+	<table>
+		<thead>
+			<tr>
+				<th>First Header</th>
+				<th>Second Header</th>
+				<th>Third Header</th>
+				<th>Fourth Header</th>
+				<th>Fifth Header</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Normal</td>
+				<td>Table</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<h3>Usage</h3>
+<pre><code>&lt;table class="<strong>tabular</strong>"&gt;
+	&lt;thead&gt;
+		&lt;tr&gt;
+			&lt;th&gt;First Header&lt;/th&gt;
+			&lt;th&gt;Second Header&lt;/th&gt;
+			&lt;th&gt;Third Header&lt;/th&gt;
+			&lt;th&gt;Fourth Header&lt;/th&gt;
+		&lt;/tr&gt;
+	&lt;/thead&gt;
+	&lt;tbody&gt;
+		&lt;tr&gt;
+			&lt;td&gt;This&lt;/td&gt;
+			&lt;td&gt;Is&lt;/td&gt;
+			&lt;td&gt;A&lt;/td&gt;
+			&lt;td&gt;Table&lt;/td&gt;
+		&lt;/tr&gt;
+	&hellip;
+	&lt;/tbody&gt;
+&lt;/table&gt;
+</code></pre>
+
+
+
+<h2>Borderless Table (.borderless)</h2>
+<p>
+	This table is styled using the .borderless class. 
+</p>
+<div class="horizontal-scrolling">
+	<table class="borderless">
+		<thead>
+			<tr>
+				<th>First Header</th>
+				<th>Second Header</th>
+				<th>Third Header</th>
+				<th>Fourth Header</th>
+				<th>Fifth Header</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Borderless</td>
+				<td>Table</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<h3>Usage</h3>
+<pre><code>&lt;table class="<strong>tabular responsive</strong>"&gt;
+	&lt;thead&gt;
+		&lt;tr&gt;
+			&lt;th&gt;First Header&lt;/th&gt;
+			&lt;th&gt;Second Header&lt;/th&gt;
+			&lt;th&gt;Third Header&lt;/th&gt;
+			&lt;th&gt;Fourth Header&lt;/th&gt;
+			&lt;th&gt;Fifth Header&lt;/th&gt;
+		&lt;/tr&gt;
+	&lt;/thead&gt;
+	&lt;tbody&gt;
+		&lt;tr&gt;
+			&lt;td&gt;This&lt;/td&gt;
+			&lt;td&gt;Is&lt;/td&gt;
+			&lt;td&gt;A&lt;/td&gt;
+			&lt;td&gt;Responsive&lt;/td&gt;
+			&lt;td&gt;Table&lt;/td&gt;
+		&lt;/tr&gt;
+	&hellip;
+	&lt;/tbody&gt;
+&lt;/table&gt;
+</code></pre>
+
+
+<h2>Responsive Table (.horizontal-scrolling)</h2>
+<p>
+	This table has been made responsive by adding the .horizontal-scrolling
+	class to a div wrapper element.
+</p>
+
+<div class="horizontal-scrolling">
+	<table>
+		<thead>
+			<tr>
+				<th>First Header</th>
+				<th>Second Header</th>
+				<th>Third Header</th>
+				<th>Fourth Header</th>
+				<th>Fifth Header</th>
+			</tr>
+		</thead>
+		<tbody>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+			<tr>
+				<td>This</td>
+				<td>Is</td>
+				<td>A</td>
+				<td>Responsive</td>
+				<td>Table</td>
+			</tr>
+		</tbody>
+	</table>
+</div>
+<h3>Usage</h3>
+<pre><code>&lt;div class="horizontal-scrolling"&gt;
+	&lt;table class="<strong>tabular responsive</strong>"&gt;
+		&lt;thead&gt;
+			&lt;tr&gt;
+				&lt;th&gt;First Header&lt;/th&gt;
+				&lt;th&gt;Second Header&lt;/th&gt;
+				&lt;th&gt;Third Header&lt;/th&gt;
+				&lt;th&gt;Fourth Header&lt;/th&gt;
+				&lt;th&gt;Fifth Header&lt;/th&gt;
+			&lt;/tr&gt;
+		&lt;/thead&gt;
+		&lt;tbody&gt;
+			&lt;tr&gt;
+				<strong>&lt;th&gt;</strong>This<strong>&lt;/th&gt;</strong>
+				&lt;td&gt;Is&lt;/td&gt;
+				&lt;td&gt;A&lt;/td&gt;
+				&lt;td&gt;Responsive&lt;/td&gt;
+				&lt;td&gt;Table&lt;/td&gt;
+			&lt;/tr&gt;
+		&hellip;
+		&lt;/tbody&gt;
+	&lt;/table&gt;
+&lt;/div&gt;
+</code></pre>
