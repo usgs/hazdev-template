1e65127d7ee0325eb78bb34b10f84d2c7e1289d2
ehunter-usgs <ehunter@usgs.gov>
2015-10-07 16:03:34 -0600

    Update postcss to use src and dest instead of file arrays

--- gruntconfig/postcss.js
+++ gruntconfig/postcss.js
@@ -5,7 +5,7 @@ var autoprefixer = require('autoprefixer'),
     calc = require('postcss-calc'),
     precss = require('precss');
 
-//var config = require('./config');
+var config = require('./config');
 
 var postcss = {
 
@@ -15,59 +15,32 @@ var postcss = {
       processors: [
         calc(),
         precss(),
-        autoprefixer({'browsers': 'last 2 versions'}), // vendor prefix
+        autoprefixer({'browsers': 'last 2 versions'})
       ]
     },
-    files: {
-      '.build/src/htdocs/css/alert.css':
-          'src/htdocs/css/alert.scss',
-      '.build/src/htdocs/css/flexible-grid.css':
-          'src/htdocs/css/flexible-grid.scss',
-      '.build/src/htdocs/css/jumplist.css':
-          'src/htdocs/css/jumplist.scss',
-      '.build/src/htdocs/css/references.css':
-          'src/htdocs/css/references.scss',
-      '.build/src/htdocs/css/responsive-table.css':
-          'src/htdocs/css/responsive-table.scss',
-
-      '.build/src/htdocs/site/default/index.css':
-          'src/htdocs/site/default/index.scss',
-      '.build/src/htdocs/site/earthquake/index.css':
-          'src/htdocs/site/earthquake/index.scss',
-      '.build/src/htdocs/site/geomag/index.css':
-          'src/htdocs/site/geomag/index.scss',
-      '.build/src/htdocs/site/landslides/index.css':
-          'src/htdocs/site/landslides/index.scss'
-    }
+    expand: true,
+    cwd: config.src + '/htdocs',
+    src: [
+      '**/*.scss',
+      '!**/_*.scss'
+    ],
+    dest: config.build + '/' + config.src + '/htdocs',
+    ext: '.css',
+    extDot: 'last'
   },
 
   dist: {
     options: {
       processors: [
-        cssnano({zindex: false}) // minify
+        cssnano({zindex: false})
       ]
     },
-    files: {
-      'dist/htdocs/css/alert.css':
-          '.build/src/htdocs/css/alert.css',
-      'dist/htdocs/css/flexible-grid.css':
-          '.build/src/htdocs/css/flexible-grid.css',
-      'dist/htdocs/css/jumplist.css':
-          '.build/src/htdocs/css/jumplist.css',
-      'dist/htdocs/css/references.css':
-          '.build/src/htdocs/css/references.css',
-      'dist/htdocs/css/responsive-table.css':
-          '.build/src/htdocs/css/responsive-table.css',
-
-      'dist/htdocs/site/default/index.css':
-          '.build/src/htdocs/site/default/index.css',
-      'dist/htdocs/site/earthquake/index.css':
-          '.build/src/htdocs/site/earthquake/index.css',
-      'dist/htdocs/site/geomag/index.css':
-          '.build/src/htdocs/site/geomag/index.css',
-      'dist/htdocs/site/landslides/index.css':
-          '.build/src/htdocs/site/landslides/index.css'
-    }
+    expand: true,
+    cwd: config.build + '/' + config.src + '/htdocs',
+    src: [
+      '**/*.css'
+    ],
+    dest: config.dist + '/htdocs'
   }
 };
 
