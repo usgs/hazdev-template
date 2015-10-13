0742a7aea5fd0feea4bf8d4ff67884ab171d8df8
Hal Simpson <hasimpson@usgs.gov>
2015-10-13 10:26:51 -0600

    Added bundle.js for tests

--- gruntconfig/concat.js
+++ gruntconfig/concat.js
@@ -10,6 +10,14 @@ var concat = {
     ],
     dest: config.build + '/' + config.src + '/htdocs/js/index.js',
     sourceMapStyle: 'inline'
+  },
+  bundle: {
+    src: [
+      config.src + '/htdocs/js/OffCanvas.js',
+      config.src + '/htdocs/js/LinkUpdater.js'
+    ],
+    dest: config.build + '/' + config.test + '/bundle.js',
+    sourceMapStyle: 'inline'
   }
 };
 
