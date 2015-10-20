4cc4a2aec88c875b858eb95839b115a94d9769e8
Eric Martinez <emartinez@usgs.gov>
2015-10-20 10:18:54 -0600

    Keep dist connect alive.

--- Gruntfile.js
+++ Gruntfile.js
@@ -79,7 +79,7 @@ module.exports = function (grunt) {
   grunt.registerTask('dist', [
     'builddist',
     'configureRewriteRules',
-    'connect:dist'
+    'connect:dist:keepalive'
   ]);
 
   // develop
