24dde0e0fb3fe3fc22fafc684ba9b85ce8cc0e08
Scott Haefner <shaefner@usgs.gov>
2017-03-14 13:52:00 -0700

    allow user to set <title> independently from <h1>

--- src/lib/layout.inc.php
+++ src/lib/layout.inc.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <title><?php echo strip_tags($TITLE); ?></title>
+  <title><?php echo strip_tags($TITLETAG); ?></title>
   <meta charset="utf-8"/>
   <meta name="viewport" content="width=device-width, initial-scale=1"/>
   <?php
