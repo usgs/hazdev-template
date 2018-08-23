88fb5a9f69fa70abd52f953248c56dac8a685e3f
ehunter-usgs <ehunter@usgs.gov>
2018-08-23 15:39:34 -0600

    Add components for hazdev-ng-template project

--- projects/hazdev-ng-template/src/lib/hazdev-ng-template.module.ts
+++ projects/hazdev-ng-template/src/lib/hazdev-ng-template.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 import { HazdevNgTemplateComponent } from './hazdev-ng-template.component';
+import { FooterComponent } from './footer/footer.component';
+import { HeaderComponent } from './header/header.component';
+import { NavigationComponent } from './navigation/navigation.component';
+import { PageComponent } from './page/page.component';
+import { SidenavComponent } from './sidenav/sidenav.component';
 
 @NgModule({
   imports: [
   ],
-  declarations: [HazdevNgTemplateComponent],
+  declarations: [HazdevNgTemplateComponent, FooterComponent, HeaderComponent, NavigationComponent, PageComponent, SidenavComponent],
   exports: [HazdevNgTemplateComponent]
 })
 export class HazdevNgTemplateModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,28 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RouterModule } from '@angular/router';
+import { HazdevNgTemplateModule } from 'hazdev-ng-template';
 
 import { AppComponent } from './app.component';
 import { HazdevTemplateModule } from './hazdev-template/hazdev-template.module';
-import { RouterModule } from '@angular/router';
-
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    HazdevNgTemplateModule,
     HazdevTemplateModule,
 
     RouterModule.forRoot([])
   ],
-  exports: [
-    HazdevTemplateModule
-  ],
-  providers: [
-  ],
+  exports: [HazdevTemplateModule],
+  providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
