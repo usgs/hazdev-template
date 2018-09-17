7c1343041125077e14cd23a2b728cdb54ccb3a24
Eric Martinez <emartinez@usgs.gov>
2018-09-17 14:38:59 -0600

    Showing example usage for cooperator.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,6 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  COOPERATOR = 'ci';
+
   TITLE = 'Hazdev Angular Template';
 
   NAVIGATION = [
