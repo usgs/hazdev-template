3a28ca61df2382e8eb03cbf00f2f70855a391cc8
Eric Martinez <emartinez@usgs.gov>
2018-09-18 19:18:25 -0600

    Fixing test to pass.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -9,7 +9,7 @@ describe('AppComponent', () => {
       declarations: [
         AppComponent,
 
-        MockComponent({selector: 'hazdev-template', inputs: ['TITLE', 'NAVIGATION', 'SITE_SITENAV', 'SITE_COMMONNAV']}),
+        MockComponent({selector: 'hazdev-template', inputs: ['TITLE', 'COOPERATOR', 'NAVIGATION', 'SITE_SITENAV', 'SITE_COMMONNAV']}),
       ],
     }).compileComponents();
   }));
